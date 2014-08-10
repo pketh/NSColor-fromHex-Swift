961630c1d8cbe843f628b781865f83acc9321a79
Pirijan <pirijan@gmail.com>
2014-08-10 15:46:19 -0400

    updated to be compatible with changes in Swift beta 5

--- NSColor+fromHex.swift
+++ NSColor+fromHex.swift
@@ -1,17 +1,21 @@
 //
 //  NSColor+hex.swift
+//  Tello
 //
 //  Created by Pirijan on 2014-07-08.
+//  Copyright (c) 2014 pketh. All rights reserved.
 //
+//  ♥︎ https://github.com/pketh/NSColor-hex-Swift
 //
+
 
 import Cocoa
 
 extension NSColor {
-    class func hex(hexColor: String) -> NSColor {
+    class func fromHex(hexColor: String) -> NSColor {
         var hex = String()
         if hexColor.hasPrefix("#") {
-            hex = hexColor.substringFromIndex(1)
+            hex = hexColor[1]
         } else {
             hex = hexColor
         }
@@ -24,12 +28,30 @@ extension NSColor {
             return colorValue / 255
         }
 
-        let redComponent = hexToCGFloat(hex.substringToIndex(2)),
-        greenComponent = hexToCGFloat(hex.substringFromIndex(2).substringToIndex(2)),
-        blueComponent = hexToCGFloat(hex.substringFromIndex(4).substringToIndex(2))
+        let redComponent = hexToCGFloat(hex[0...1]),
+        greenComponent = hexToCGFloat(hex[2...3]),
+        blueComponent = hexToCGFloat(hex[4...5])
 
         let color = NSColor(calibratedRed: redComponent, green: greenComponent, blue: blueComponent, alpha: 1)
 
         return color
     }
 }
+
+extension String {
+    subscript (i: Int) -> String {
+        return String(Array(self)[i])
+    }
+    subscript (r: Range<Int>) -> String {
+        var start = advance(startIndex, r.startIndex)
+            var end = advance(startIndex, r.endIndex)
+            return substringWithRange(Range(start: start, end: end))
+    }
+}
+
+
+// ## sample usage
+// var hexColor = "#259B9A"
+// NSColor.fromHex(hexColor)
+// returns an NSColor object:
+// `NSColor(calibratedRed: 0.8, green: 0.322, blue: 0.322, alpha: 1)`
